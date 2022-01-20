e8fdedc9db6e5130878560dd0aa05938f455428e
Leslie Counterman <LCounterman1@gmail.com>
2022-01-20 18:53:07 -0500

    notedetail

--- ElevenNote.MVC/Controllers/NoteController.cs
+++ ElevenNote.MVC/Controllers/NoteController.cs
@@ -1,4 +1,5 @@
-﻿using ElevenNote.Models;
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
 using ElevenNote.Services;
 using Microsoft.AspNet.Identity;
 using System;
@@ -49,6 +50,28 @@ namespace ElevenNote.MVC.Controllers
 
             ModelState.AddModelError("", "Note could not be created.");
 
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var service = CreateNoteService();
+            var detail = service.GetNoteById(id);
+            var model =
+                new NoteEdit
+                {
+                    NoteId = detail.NoteId,
+                    Title = detail.Title,
+                    Content = detail.Content
+                };
             return View(model);
         }
 
